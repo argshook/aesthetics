aed49b156975ca411f42d71b37724da3df15c3d6
argshook <argshook@gmail.com>
2016-08-01 20:24:26 +0300

    do something with screen

--- src/index.js
+++ src/index.js
@@ -1,32 +1,22 @@
 /* globals Promise, createImageBitmap */
 const canvas = document.getElementById('canvas');
 
-const screen = {
-  w: window.innerWidth,
-  h: window.innerHeight
-};
-
-canvas.setAttribute('width', screen.w);
-canvas.setAttribute('height', screen.h);
+const screen = new Screen();
 
 const ctx = canvas.getContext('2d');
 
 loadImg('assets/mullet.jpg')
-  .then(img => {
-    ctx.drawImage(img, 0, 0);
-
-    const imageData = ctx.getImageData(0, 0, screen.w, screen.h);
-    ctx.clearRect(0, 0, screen.w, screen.h);
-
-    return getKeyedImageData(imageData, {
+  .then(getImageData)
+  .then(imageData =>
+    removeAlphaFromImageData(imageData, {
       red: imageData.data[0],
       green: imageData.data[1],
       blue: imageData.data[2]
-    });
-  })
-  .then(keyedImageData => {
-    return createImageBitmap(keyedImageData, 0, 0, screen.w, screen.h)
-  })
+    }
+  ))
+
+  .then(keyedImageData => createImageBitmap(keyedImageData, 0, 0, screen.w, screen.h))
+
   .then(sprite => {
     canvas.addEventListener('mousemove', ({ layerX: x, layerY: y }) => {
       window.requestAnimationFrame(() => {
@@ -36,6 +26,24 @@ loadImg('assets/mullet.jpg')
     });
   });
 
+function Screen() {
+  this.w = window.innerWidth;
+  this.h = window.innerHeight;
+
+  let setCanvasSize = function() {
+    let { innerWidth: w, innerHeight: h } = window;
+
+    this.w = w;
+    this.h = h;
+
+    canvas.setAttribute('width', w);
+    canvas.setAttribute('height', h);
+  }.bind(this);
+
+  window.addEventListener('resize', setCanvasSize);
+
+  setCanvasSize();
+}
 
 function loadImg(src) {
   return new Promise((resolve, reject) => {
@@ -46,14 +54,23 @@ function loadImg(src) {
   });
 }
 
-function getKeyedImageData(imageData, key) {
+function getImageData(imageNode) {
+  ctx.drawImage(imageNode, 0, 0);
+
+  const imageData = ctx.getImageData(0, 0, screen.w, screen.h);
+  ctx.clearRect(0, 0, screen.w, screen.h);
+
+  return Promise.resolve(imageData);
+}
+
+function removeAlphaFromImageData(imageData, colorKey) {
   const tolerance = 230;
 
   for (let i = 0, n = imageData.data.length; i < n; i += 4) {
     let diff =
-      Math.abs(imageData.data[i] - key.red) +
-      Math.abs(imageData.data[i + 1] - key.green) +
-      Math.abs(imageData.data[i + 2] - key.blue);
+      Math.abs(imageData.data[i] - colorKey.red) +
+      Math.abs(imageData.data[i + 1] - colorKey.green) +
+      Math.abs(imageData.data[i + 2] - colorKey.blue);
 
     if (diff < tolerance) {
       // set alpha channel
